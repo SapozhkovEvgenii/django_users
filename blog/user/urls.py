0de6488b60d4fa1b809bea14432f64165ba97041
Sapozhkov_Evgenii <sapozhkovevgenii@gmail.com>
2022-02-28 14:03:19 +0300

    Refactoring

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -1,11 +1,11 @@
 from unicodedata import name
 from django.shortcuts import redirect
 from django.urls import path
-from user.views import UsersView, login, register
+from user.views import UsersView, login, RegisterView
 urlpatterns = [
     path("", lambda request: redirect("all_users")),
     path("login", login, name="user_login"),
     path("users/", UsersView.as_view(), name="all_users"),
-    path("register", register, name="register_user"),
+    path("register", RegisterView.as_view(), name="register_user"),
 ]
 
--- blog/user/views.py
+++ blog/user/views.py
@@ -1,15 +1,27 @@
+from audioop import reverse
 from multiprocessing import context
 from django.shortcuts import render, HttpResponse, redirect
 from user.models import User
 from user.forms import LoginForm, RegisterForm
 import json
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
 
 
 class UsersView(ListView):
     paginate_by = 2
     model = User
     template_name = "users.html"
+
+
+class RegisterView(FormView):
+    template_name = "register.html"
+    form_class = RegisterForm
+    success_url = "/user/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
 
 
 def login(request):
@@ -22,15 +34,15 @@ def login(request):
         context.update(login_form=login_form)
     return render(request, "login.html", context)
 
-def register(request):
-    context = {"register_form": RegisterForm}
-    if request.method == "POST":
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid:
-            register_form.save()
-            return redirect("all_users")
-        context.update(register_form=register_form)
-    return render(request, "register.html", context)
+# def register(request):
+#     context = {"register_form": RegisterForm}
+#     if request.method == "POST":
+#         register_form = RegisterForm(request.POST)
+#         if register_form.is_valid:
+#             register_form.save()
+#             return redirect("all_users")
+#         context.update(register_form=register_form)
+#     return render(request, "register.html", context)
 
 def user(request):
     context = {"key": "value", "result": "True"}
